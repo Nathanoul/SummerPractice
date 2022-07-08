d25096508a0301f7380f941bc4782868995447d1
Nathanoul <92688201+Nathanoul@users.noreply.github.com>
2022-07-09 02:12:15 +0300

    Update MsgBoxListener.py

--- Executor/MsgBoxListener.py
+++ Executor/MsgBoxListener.py
@@ -14,20 +14,19 @@ import win32con
 
 
 class MsgBoxListener(Thread):
-
     def __init__(self, title: str, interval: int):
         Thread.__init__(self)
         self._title = title
         self._interval = interval
-        self._stop_event = Event()
+        self._stopevent = Event()
         self._message = ''
 
     def stop(self):
-        self._stop_event.set()
+        self._stopevent.set()
 
     @property
-    def IsRunning(self):
-        return not self._stopevent.isset()
+    def isrunning(self):
+        return not self._stopevent.is_set()
 
     def run(self):
         while self.isrunning:
@@ -37,23 +36,23 @@ class MsgBoxListener(Thread):
             except Exception as e:
                 print(e, flush=True)
 
-    def _CloseMsgbox(self):
+    def _closemsgbox(self):
         # find the top window by title
         hwnd = win32gui.FindWindow(None, self._title)
         if not hwnd: return
 
         # find child button
-        hbtn = win32gui.FindWindowEx(hwnd, None, 'Button', None)
-        hmsg = win32gui.FindWindowEx(hwnd, 0, 'Static', None)
-        if not hbtn: return
+        h_btn = win32gui.FindWindowEx(hwnd, None, 'Button', None)
+        h_msg = win32gui.FindWindowEx(hwnd, 0, 'Static', None)
+        if not h_btn: return
 
         # save text
-        self._message = win32gui.GetWindowText(hmsg)
+        self._message = win32gui.GetWindowText(h_msg)
 
         # click button
-        win32gui.PostMessage(hbtn, win32con.WM_LBUTTONDOWN, None, None)
+        win32gui.PostMessage(h_btn, win32con.WM_LBUTTONDOWN, None, None)
         time.sleep(0.2)
-        win32gui.PostMessage(hbtn, win32con.WM_LBUTTONUP, None, None)
+        win32gui.PostMessage(h_btn, win32con.WM_LBUTTONUP, None, None)
         time.sleep(0.2)
 
     def GetMessage(self):
